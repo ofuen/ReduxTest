d3ada561c62d213e071aa49841e88f9b7e1f065a
oscar <oscar@o2webdev.com>
2019-08-10 11:26:14 -0400

    Fixed cannot fixed module

--- reduxexample/src/store.js
+++ reduxexample/src/store.js
@@ -1,18 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = createStore(
-    rootReducer, 
-    initialState, 
-    compose(
-     applyMiddleware(...middleware),
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-    );
-
-export default store;
+import { createStore } from "redux";
+import rotateReducer from "reducers/rotateReducer";
+function configureStore(state = { rotating: true }) {
+    return createStore(rotateReducer, state);
+}
+export default configureStore;
