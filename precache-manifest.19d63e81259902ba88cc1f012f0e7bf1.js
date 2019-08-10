e7bc2566ec1fc19b8df5b0402c36ef7220e4f52c
oscar <oscar@o2webdev.com>
2019-08-10 10:29:01 -0400

    Updates

---
+++ precache-manifest.19d63e81259902ba88cc1f012f0e7bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf1f0b4072dd6a710102d9ce2f750e7",
+    "url": "/ReduxTest/index.html"
+  },
+  {
+    "revision": "6ae00d843532f747b8f8",
+    "url": "/ReduxTest/static/css/main.e3d6134f.chunk.css"
+  },
+  {
+    "revision": "94bab8242a9ca84d22f4",
+    "url": "/ReduxTest/static/js/2.69f9d809.chunk.js"
+  },
+  {
+    "revision": "6ae00d843532f747b8f8",
+    "url": "/ReduxTest/static/js/main.09827fe8.chunk.js"
+  },
+  {
+    "revision": "62b086751df01588865e",
+    "url": "/ReduxTest/static/js/runtime~main.aa0eaa21.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReduxTest/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReduxTest/precache-manifest.bdf2a780156835c5757c0f4be9a516bf.js"
+  "/ReduxTest/precache-manifest.19d63e81259902ba88cc1f012f0e7bf1.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ReduxTest/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReduxTest/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
